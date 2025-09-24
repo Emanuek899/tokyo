fa045930ab4e7ecd846cad44cadde6eff4074650
Ralsei <emanuel.carbajalsantacruz@gmail.com>
2025-09-23 18:19:39 -0600

    <Implementation> bootstrap cdn

--- vistas/partials/head.php
+++ vistas/partials/head.php
@@ -8,6 +8,7 @@
 <title><?= htmlspecialchars($pageTitle) ?></title>
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link rel="preload" as="style" href="assets/css/tokens.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <link rel="stylesheet" href="assets/css/tokens.css">
 <link rel="stylesheet" href="assets/css/base.css">
 <link rel="stylesheet" href="assets/css/utilities.css">
